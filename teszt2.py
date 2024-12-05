8eee956342bd78d9f9eac621f84e8f6627219d82
Mankoka <mariann.aleva@gmail.com>
2024-12-05 18:53:49 +0100

    gyakorlás2

--- teszt2.py
+++ teszt2.py
@@ -18,8 +18,16 @@ def osszeAd():
         sum = sum + i
     return sum
 
+def legnagyobbelem():
+    max = veletlen()[0]
+    for i in veletlen():
+        if i > max:
+            max = i
+    return max
+
 veletlen()
 kiir()  # Csak a `kiir()` meghívása szükséges, mert az magában hívja a `veletlen()`-t
 print()
 print(f"A lista elemeinek összege: {osszeAd()}")
+print(f"A lista elemeinek legnagyobb értéke: {legnagyobbelem()}")
 
